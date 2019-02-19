55c82dcbc63f7b95b8b814a1aa05efd9da3be338
cjonasl <jonas5669@outlook.com>
2019-02-19 16:34:43 +0000

    Commit 2019-02-19 kl. 16:34

---
+++ Deployment/Azure/CarlJonas/AddressBook/Models/PersonInfo.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AddressBook
+{
+    public class PersonInfo
+    {
+        public int PersonId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public string DateOfBirth { get; set; }
+        public string Phone { get; set; }
+        public string Address { get; set; }
+        public string Town { get; set; }
+        public string PostCode { get; set; }
+        public string Country { get; set; }
+        public bool IsCloseFriend { get; set; } //Default is false
+
+        public PersonInfo() { }
+
+        public PersonInfo
+            (
+              int personId,
+              string firstName,
+              string lastName,
+              string gender,
+              string dateOfBirth,
+              string phone,
+              string address,
+              string town,
+              string postCode,
+              string country,
+              bool isCloseFriend
+            )
+        {
+            this.PersonId = personId;
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Gender = gender;
+            this.DateOfBirth = dateOfBirth;
+            this.Phone = phone;
+            this.Address = address;
+            this.Town = town;
+            this.PostCode = postCode;
+            this.Country = country;
+            this.IsCloseFriend = isCloseFriend;
+        }
+
+        public PersonInfo(int id, string firstName, string lastName) :
+            this(id, firstName, lastName, null, null, null, null, null, null, null, false)
+        { }
+    }
+}
---
+++ Deployment/Azure/CarlJonas/AddressBook/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AddressBook")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Hewlett-Packard")]
+[assembly: AssemblyProduct("AddressBook")]
+[assembly: AssemblyCopyright("Copyright © Hewlett-Packard 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("720333b4-76a1-4ced-81b6-23a021f83650")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
