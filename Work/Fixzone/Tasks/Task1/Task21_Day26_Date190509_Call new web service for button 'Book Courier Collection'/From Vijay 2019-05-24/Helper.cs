e54aec006335ee3739b002062c5bae25d39f83e8
cjonasl <jonas5669@outlook.com>
2019-05-29 09:16:18 +0100

    Commit 2019-05-29 kl. 09:16

---
+++ Work/Fixzone/Tasks/Task1/Task21_Day26_Date190509_Call new web service for button 'Book Courier Collection'/From Vijay 2019-05-24/Helper.cs
@@ -0,0 +1,506 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Web;
+
+namespace Mobile.Portal.Helpers
+{
+   
+    public static  class Helper
+    {
+ public static readonly List<string> ImageExtensions = new List<string> { ".JPG", ".JPEG", ".JPE", ".BMP", ".GIF", ".PNG", ".TIF", ".TIFF" };
+
+        public static string DisplayObjectInfo(Object o)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            // Include the type of the object
+            System.Type type = o.GetType();
+            sb.Append("Type: " + type.Name);
+
+            // Include information for each Field
+            sb.Append("\r\n\r\nFields:");
+            FieldInfo[] fi = type.GetFields();
+            if (fi.Length > 0)
+            {
+                foreach (FieldInfo f in fi)
+                {
+                    sb.Append("\r\n " + f.ToString() + " = " + f.GetValue(o));
+                }
+            }
+            else
+                sb.Append("\r\n None");
+
+            // Include information for each Property
+            sb.Append("\r\n\r\nProperties:");
+            System.Reflection.PropertyInfo[] pi = type.GetProperties();
+            if (pi.Length > 0)
+            {
+                foreach (PropertyInfo p in pi)
+                {
+                    sb.Append("\r\n " + p.ToString() + " = " +
+                              p.GetValue(o, null));
+                }
+            }
+            else
+                sb.Append("\r\n None");
+
+            return sb.ToString();
+        }
+
+        private static readonly Dictionary<string, string> MIMETypesDictionary = new Dictionary<string, string>
+  {
+       {"7z","application/x-7z-compressed"},
+    {"ai", "application/postscript"},
+    {"aif", "audio/x-aiff"},
+    {"aifc", "audio/x-aiff"},
+    {"aiff", "audio/x-aiff"},
+    {"asc", "text/plain"},
+    {"atom", "application/atom+xml"},
+    {"au", "audio/basic"},
+    {"avi", "video/x-msvideo"},
+    {"bcpio", "application/x-bcpio"},
+    {"bin", "application/octet-stream"},
+    {"bmp", "image/bmp"},
+    {"cdf", "application/x-netcdf"},
+    {"cgm", "image/cgm"},
+    {"class", "application/octet-stream"},
+    {"cpio", "application/x-cpio"},
+    {"cpt", "application/mac-compactpro"},
+    {"csh", "application/x-csh"},
+    {"css", "text/css"},
+    {"csv", "text/csv"},
+    {"dcr", "application/x-director"},
+    {"dat", "application/octet-stream"},
+    {"dif", "video/x-dv"},
+    {"dir", "application/x-director"},
+    {"djv", "image/vnd.djvu"},
+    {"djvu", "image/vnd.djvu"},
+    {"dll", "application/octet-stream"},
+    {"dmg", "application/octet-stream"},
+    {"dms", "application/octet-stream"},
+     {"dmp", "application/octet-stream"},
+    {"doc", "application/msword"},
+    {"docx","application/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+    {"dotx", "application/vnd.openxmlformats-officedocument.wordprocessingml.template"},
+    {"docm","application/vnd.ms-word.document.macroEnabled.12"},
+    {"dotm","application/vnd.ms-word.template.macroEnabled.12"},
+    {"dtd", "application/xml-dtd"},
+    {"dv", "video/x-dv"},
+    {"dvi", "application/x-dvi"},
+  {"dxr", "application/x-director"},
+  {"eps", "application/postscript"},
+  {"etx", "text/x-setext"},
+    {"exe", "application/octet-stream"},
+    {"ez", "application/andrew-inset"},
+    {"gif", "image/gif"},
+    {"gram", "application/srgs"},
+    {"grxml", "application/srgs+xml"},
+    {"gtar", "application/x-gtar"},
+    {"hdf", "application/x-hdf"},
+    {"hqx", "application/mac-binhex40"},
+    {"htm", "text/html"},
+    {"html", "text/html"},
+    {"ice", "x-conference/x-cooltalk"},
+    {"ico", "image/x-icon"},
+        {"ini", "text/plain"},
+    {"ics", "text/calendar"},
+    {"ief", "image/ief"},
+    {"ifb", "text/calendar"},
+    {"iges", "model/iges"},
+    {"igs", "model/iges"},
+    {"jnlp", "application/x-java-jnlp-file"},
+    {"jp2", "image/jp2"},
+    {"jpe", "image/jpeg"},
+    {"jpeg", "image/jpeg"},
+    {"jpg", "image/jpeg"},
+    {"js", "application/x-javascript"},
+    {"kar", "audio/midi"},
+    {"latex", "application/x-latex"},
+    {"lha", "application/octet-stream"},
+      {"log", "text/plain"},
+    {"lzh", "application/octet-stream"},
+    {"m3u", "audio/x-mpegurl"},
+    {"m4a", "audio/mp4a-latm"},
+    {"m4b", "audio/mp4a-latm"},
+    {"m4p", "audio/mp4a-latm"},
+    {"m4u", "video/vnd.mpegurl"},
+    {"m4v", "video/x-m4v"},
+    {"mac", "image/x-macpaint"},
+    {"man", "application/x-troff-man"},
+    {"mathml", "application/mathml+xml"},
+    {"me", "application/x-troff-me"},
+    {"mesh", "model/mesh"},
+    {"mid", "audio/midi"},
+    {"midi", "audio/midi"},
+    {"mif", "application/vnd.mif"},
+    {"mov", "video/quicktime"},
+    {"movie", "video/x-sgi-movie"},
+    {"mp2", "audio/mpeg"},
+    {"mp3", "audio/mpeg"},
+    {"mp4", "video/mp4"},
+    {"mpe", "video/mpeg"},
+    {"mpeg", "video/mpeg"},
+    {"mpg", "video/mpeg"},
+    {"mpga", "audio/mpeg"},
+    {"ms", "application/x-troff-ms"},
+    {"msh", "model/mesh"},
+    {"mxu", "video/vnd.mpegurl"},
+    {"nc", "application/x-netcdf"},
+    {"oda", "application/oda"},
+    {"ogg", "application/ogg"},
+    {"pbm", "image/x-portable-bitmap"},
+    {"pct", "image/pict"},
+    {"pdb", "chemical/x-pdb"},
+    {"pdf", "application/pdf"},
+    {"pgm", "image/x-portable-graymap"},
+    {"pgn", "application/x-chess-pgn"},
+    {"pic", "image/pict"},
+    {"pict", "image/pict"},
+    {"png", "image/png"}, 
+    {"pnm", "image/x-portable-anymap"},
+    {"pnt", "image/x-macpaint"},
+    {"pntg", "image/x-macpaint"},
+    {"ppm", "image/x-portable-pixmap"},
+    {"ppt", "application/vnd.ms-powerpoint"},
+    {"pptx","application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+    {"potx","application/vnd.openxmlformats-officedocument.presentationml.template"},
+    {"ppsx","application/vnd.openxmlformats-officedocument.presentationml.slideshow"},
+    {"ppam","application/vnd.ms-powerpoint.addin.macroEnabled.12"},
+    {"pptm","application/vnd.ms-powerpoint.presentation.macroEnabled.12"},
+    {"potm","application/vnd.ms-powerpoint.template.macroEnabled.12"},
+    {"ppsm","application/vnd.ms-powerpoint.slideshow.macroEnabled.12"},
+    {"ps", "application/postscript"},
+    {"qt", "video/quicktime"},
+    {"qti", "image/x-quicktime"},
+    {"qtif", "image/x-quicktime"},
+    {"ra", "audio/x-pn-realaudio"},
+    {"ram", "audio/x-pn-realaudio"},
+     {"rar", "application/x-rar-compressed"},
+    {"ras", "image/x-cmu-raster"},
+    {"rdf", "application/rdf+xml"},
+    {"rgb", "image/x-rgb"},
+    {"rm", "application/vnd.rn-realmedia"},
+    {"roff", "application/x-troff"},
+    {"rtf", "text/rtf"},
+    {"rtx", "text/richtext"},
+    {"sgm", "text/sgml"},
+    {"sgml", "text/sgml"},
+    {"sh", "application/x-sh"},
+    {"shar", "application/x-shar"},
+    {"silo", "model/mesh"},
+    {"sit", "application/x-stuffit"},
+    {"skd", "application/x-koan"},
+    {"skm", "application/x-koan"},
+    {"skp", "application/x-koan"},
+    {"skt", "application/x-koan"},
+    {"smi", "application/smil"},
+    {"smil", "application/smil"},
+    {"snd", "audio/basic"},
+    {"so", "application/octet-stream"},
+    {"spl", "application/x-futuresplash"},
+    {"src", "application/x-wais-source"},
+    {"sv4cpio", "application/x-sv4cpio"},
+    {"sv4crc", "application/x-sv4crc"},
+    {"svg", "image/svg+xml"},
+    {"swf", "application/x-shockwave-flash"},
+    {"t", "application/x-troff"},
+    {"tar", "application/x-tar"},
+    {"tcl", "application/x-tcl"},
+    {"tex", "application/x-tex"},
+    {"texi", "application/x-texinfo"},
+    {"texinfo", "application/x-texinfo"},
+    {"tif", "image/tiff"},
+    {"tiff", "image/tiff"},
+    {"tr", "application/x-troff"},
+    {"tsv", "text/tab-separated-values"},
+    {"txt", "text/plain"},
+    {"ustar", "application/x-ustar"},
+    {"vcd", "application/x-cdlink"},
+    {"vrml", "model/vrml"},
+    {"vxml", "application/voicexml+xml"},
+    {"wav", "audio/x-wav"},
+    {"wbmp", "image/vnd.wap.wbmp"},
+    {"wbmxl", "application/vnd.wap.wbxml"},
+    {"wml", "text/vnd.wap.wml"},
+    {"wmlc", "application/vnd.wap.wmlc"},
+    {"wmls", "text/vnd.wap.wmlscript"},
+    {"wmlsc", "application/vnd.wap.wmlscriptc"},
+    {"wrl", "model/vrml"},
+    {"xbm", "image/x-xbitmap"},
+    {"xht", "application/xhtml+xml"},
+    {"xhtml", "application/xhtml+xml"},
+    {"xls", "application/vnd.ms-excel"},                        
+    {"xml", "application/xml"},
+    {"xpm", "image/x-xpixmap"},
+    {"xsl", "application/xml"},
+    {"xlsx","application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+    {"xltx","application/vnd.openxmlformats-officedocument.spreadsheetml.template"},
+    {"xlsm","application/vnd.ms-excel.sheet.macroEnabled.12"},
+    {"xltm","application/vnd.ms-excel.template.macroEnabled.12"},
+    {"xlam","application/vnd.ms-excel.addin.macroEnabled.12"},
+    {"xlsb","application/vnd.ms-excel.sheet.binary.macroEnabled.12"},
+    {"xslt", "application/xslt+xml"},
+    {"xul", "application/vnd.mozilla.xul+xml"},
+    {"xwd", "image/x-xwindowdump"},
+    {"xyz", "chemical/x-xyz"},
+    {"zip", "application/zip"}
+  };
+
+        private static readonly Dictionary<string, string> MediaServiceMIMETypesDictionary = new Dictionary<string, string>
+  {
+      {"7z","document/x-7z-compressed"},
+    {"ai", "application/postscript"},
+    {"aif", "audio/x-aiff"},
+    {"aifc", "audio/x-aiff"},
+    {"aiff", "audio/x-aiff"},
+    {"asc", "text/plain"},
+    {"atom", "application/atom+xml"},
+    {"au", "audio/basic"},
+    {"avi", "video/x-msvideo"},
+    {"bcpio", "application/x-bcpio"},
+    {"bin", "application/octet-stream"},
+    {"bmp", "image/bmp"},
+    {"cdf", "application/x-netcdf"},
+    {"cgm", "image/cgm"},
+    {"class", "application/octet-stream"},
+    {"cpio", "application/x-cpio"},
+    {"cpt", "application/mac-compactpro"},
+    {"csh", "application/x-csh"},
+    {"css", "text/css"},
+    {"csv", "document/csv"},
+    {"dcr", "application/x-director"},
+    {"dif", "video/x-dv"},
+    {"dir", "application/x-director"},
+    {"djv", "image/vnd.djvu"},
+    {"djvu", "image/vnd.djvu"},
+       {"dat", "document/octet-stream"},
+       {"dmp", "document/octet-stream"},
+    {"dll", "application/octet-stream"},
+    {"dmg", "application/octet-stream"},
+    {"dms", "application/octet-stream"},
+    {"doc", "document/msword"},
+    {"docx","document/vnd.openxmlformats-officedocument.wordprocessingml.document"},
+    {"dotx", "document/vnd.openxmlformats-officedocument.wordprocessingml.template"},
+    {"docm","document/vnd.ms-word.document.macroEnabled.12"},
+    {"dotm","document/vnd.ms-word.template.macroEnabled.12"},
+    {"dtd", "application/xml-dtd"},
+    {"dv", "video/x-dv"},
+    {"dvi", "application/x-dvi"},
+  {"dxr", "application/x-director"},
+  {"eps", "application/postscript"},
+  {"etx", "text/x-setext"},
+    {"exe", "application/octet-stream"},
+    {"ez", "application/andrew-inset"},
+    {"gif", "image/gif"},
+    {"gram", "application/srgs"},
+    {"grxml", "application/srgs+xml"},
+    {"gtar", "application/x-gtar"},
+    {"hdf", "application/x-hdf"},
+    {"hqx", "application/mac-binhex40"},
+    {"htm", "text/html"},
+    {"html", "text/html"},
+    {"ice", "x-conference/x-cooltalk"},
+       {"ini", "document/plain"},
+    {"ico", "image/x-icon"},
+    {"ics", "text/calendar"},
+    {"ief", "image/ief"},
+    {"ifb", "text/calendar"},
+    {"iges", "model/iges"},
+    {"igs", "model/iges"},
+    {"jnlp", "application/x-java-jnlp-file"},
+    {"jp2", "image/jp2"},
+    {"jpe", "image/jpeg"},
+    {"jpeg", "image/jpeg"},
+    {"jpg", "image/jpeg"},
+    {"js", "application/x-javascript"},
+    {"kar", "audio/midi"},
+    {"latex", "application/x-latex"},
+    {"lha", "application/octet-stream"},
+    {"log", "document/plain"},
+    {"lzh", "application/octet-stream"},
+    {"m3u", "audio/x-mpegurl"},
+    {"m4a", "audio/mp4a-latm"},
+    {"m4b", "audio/mp4a-latm"},
+    {"m4p", "audio/mp4a-latm"},
+    {"m4u", "video/vnd.mpegurl"},
+    {"m4v", "video/x-m4v"},
+    {"mac", "image/x-macpaint"},
+    {"man", "application/x-troff-man"},
+    {"mathml", "application/mathml+xml"},
+    {"me", "application/x-troff-me"},
+    {"mesh", "model/mesh"},
+    {"mid", "audio/midi"},
+    {"midi", "audio/midi"},
+    {"mif", "application/vnd.mif"},
+    {"mov", "video/quicktime"},
+    {"movie", "video/x-sgi-movie"},
+    {"mp2", "audio/mpeg"},
+    {"mp3", "audio/mpeg"},
+    {"mp4", "video/mp4"},
+    {"mpe", "video/mpeg"},
+    {"mpeg", "video/mpeg"},
+    {"mpg", "video/mpeg"},
+    {"mpga", "audio/mpeg"},
+    {"ms", "application/x-troff-ms"},
+    {"msh", "model/mesh"},
+    {"mxu", "video/vnd.mpegurl"},
+    {"nc", "application/x-netcdf"},
+    {"oda", "application/oda"},
+    {"ogg", "application/ogg"},
+    {"pbm", "image/x-portable-bitmap"},
+    {"pct", "image/pict"},
+    {"pdb", "chemical/x-pdb"},
+    {"pdf", "document/pdf"},
+    {"pgm", "image/x-portable-graymap"},
+    {"pgn", "application/x-chess-pgn"},
+    {"pic", "image/pict"},
+    {"pict", "image/pict"},
+    {"png", "image/png"}, 
+    {"pnm", "image/x-portable-anymap"},
+    {"pnt", "image/x-macpaint"},
+    {"pntg", "image/x-macpaint"},
+    {"ppm", "image/x-portable-pixmap"},
+    {"ppt", "application/vnd.ms-powerpoint"},
+    {"pptx","application/vnd.openxmlformats-officedocument.presentationml.presentation"},
+    {"potx","application/vnd.openxmlformats-officedocument.presentationml.template"},
+    {"ppsx","application/vnd.openxmlformats-officedocument.presentationml.slideshow"},
+    {"ppam","application/vnd.ms-powerpoint.addin.macroEnabled.12"},
+    {"pptm","application/vnd.ms-powerpoint.presentation.macroEnabled.12"},
+    {"potm","application/vnd.ms-powerpoint.template.macroEnabled.12"},
+    {"ppsm","application/vnd.ms-powerpoint.slideshow.macroEnabled.12"},
+    {"ps", "application/postscript"},
+    {"qt", "video/quicktime"},
+    {"qti", "image/x-quicktime"},
+    {"qtif", "image/x-quicktime"},
+    {"ra", "audio/x-pn-realaudio"},
+    {"ram", "audio/x-pn-realaudio"},
+    {"rar", "document/x-rar-compressed"},
+    {"ras", "image/x-cmu-raster"},
+    {"rdf", "application/rdf+xml"},
+    {"rgb", "image/x-rgb"},
+    {"rm", "application/vnd.rn-realmedia"},
+    {"roff", "application/x-troff"},
+    {"rtf", "document/rtf"},
+    {"rtx", "document/richtext"},
+    {"sgm", "text/sgml"},
+    {"sgml", "text/sgml"},
+    {"sh", "application/x-sh"},
+    {"shar", "application/x-shar"},
+    {"silo", "model/mesh"},
+    {"sit", "application/x-stuffit"},
+    {"skd", "application/x-koan"},
+    {"skm", "application/x-koan"},
+    {"skp", "application/x-koan"},
+    {"skt", "application/x-koan"},
+    {"smi", "application/smil"},
+    {"smil", "application/smil"},
+    {"snd", "audio/basic"},
+    {"so", "application/octet-stream"},
+    {"spl", "application/x-futuresplash"},
+    {"src", "application/x-wais-source"},
+    {"sv4cpio", "application/x-sv4cpio"},
+    {"sv4crc", "application/x-sv4crc"},
+    {"svg", "image/svg+xml"},
+    {"swf", "application/x-shockwave-flash"},
+    {"t", "application/x-troff"},
+    {"tar", "application/x-tar"},
+    {"tcl", "application/x-tcl"},
+    {"tex", "application/x-tex"},
+    {"texi", "application/x-texinfo"},
+    {"texinfo", "application/x-texinfo"},
+    {"tif", "image/tiff"},
+    {"tiff", "image/tiff"},
+    {"tr", "application/x-troff"},
+    {"tsv", "text/tab-separated-values"},
+    {"txt", "document/plain"},
+    {"ustar", "application/x-ustar"},
+    {"vcd", "application/x-cdlink"},
+    {"vrml", "model/vrml"},
+    {"vxml", "application/voicexml+xml"},
+    {"wav", "audio/x-wav"},
+    {"wbmp", "image/vnd.wap.wbmp"},
+    {"wbmxl", "application/vnd.wap.wbxml"},
+    {"wml", "text/vnd.wap.wml"},
+    {"wmlc", "application/vnd.wap.wmlc"},
+    {"wmls", "text/vnd.wap.wmlscript"},
+    {"wmlsc", "application/vnd.wap.wmlscriptc"},
+    {"wrl", "model/vrml"},
+    {"xbm", "image/x-xbitmap"},
+    {"xht", "application/xhtml+xml"},
+    {"xhtml", "application/xhtml+xml"}, 
+    {"xls", "document/vnd.ms-excel"},                        
+    {"xml", "application/xml"},
+    {"xpm", "image/x-xpixmap"},
+    {"xsl", "application/xml"},
+    {"xlsx","document/vnd.openxmlformats-officedocument.spreadsheetml.sheet"},
+    {"xltx","application/vnd.openxmlformats-officedocument.spreadsheetml.template"},
+    {"xlsm","application/vnd.ms-excel.sheet.macroEnabled.12"},
+       {"xltm","application/vnd.ms-excel.template.macroEnabled.12"},
+    {"xlam","application/vnd.ms-excel.addin.macroEnabled.12"},
+    {"xlsb","application/vnd.ms-excel.sheet.binary.macroEnabled.12"},
+    {"xslt", "application/xslt+xml"},
+    {"xul", "application/vnd.mozilla.xul+xml"},
+    {"xwd", "image/x-xwindowdump"},
+    {"xyz", "chemical/x-xyz"},
+    {"zip", "document/zip"}
+  };
+        public static string GetMIMEType(string fileName)
+        {
+            //get file extension
+            string extension = Path.GetExtension(fileName).ToLowerInvariant();
+
+            if (extension.Length > 0 &&
+                MediaServiceMIMETypesDictionary.ContainsKey(extension.Remove(0, 1)))
+            {
+                return MediaServiceMIMETypesDictionary[extension.Remove(0, 1)];
+            }
+            return "unknown/unknown";
+        }
+        public static string GetFileType(string MIMEType)
+        {
+           string[] MIMEFile= MIMEType.Split('/');
+           //if (MIMETypesDictionary.Any(kvp => kvp.key.Contains(MIMEFile[1])))
+           // {
+           //     return MIMETypesDictionary.FirstOrDefault(x => x.Value.Contains(MIMEFile[1])).Key; 
+           // }
+          
+
+            if (MIMETypesDictionary.ContainsKey(MIMEFile[1]))
+           {
+               return MIMETypesDictionary.FirstOrDefault(x => x.Key == (MIMEFile[1])).Value;
+           }
+            return "file";
+        }
+    }
+
+    public static class Ext
+    {
+        private const long OneKb = 1024;
+        private const long OneMb = OneKb * 1024;
+        private const long OneGb = OneMb * 1024;
+        private const long OneTb = OneGb * 1024;
+
+        public static string ToPrettySize(this int value, int decimalPlaces = 0)
+        {
+            return ((long)value).ToPrettySize(decimalPlaces);
+        }
+
+        public static string ToPrettySize(this long value, int decimalPlaces = 0)
+        {
+            var asTb = Math.Round((double)value / OneTb, decimalPlaces);
+            var asGb = Math.Round((double)value / OneGb, decimalPlaces);
+            var asMb = Math.Round((double)value / OneMb, decimalPlaces);
+            var asKb = Math.Round((double)value / OneKb, decimalPlaces);
+            string chosenValue = asTb > 1 ? string.Format("{0}Tb", asTb)
+                : asGb > 1 ? string.Format("{0}Gb", asGb)
+                : asMb > 1 ? string.Format("{0}Mb", asMb)
+                : asKb > 1 ? string.Format("{0}Kb", asKb)
+                : string.Format("{0}B", Math.Round((double)value, decimalPlaces));
+            return chosenValue;
+        }
+    }
+}
