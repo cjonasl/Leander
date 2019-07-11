d3d97628cd23c219f455a08bcc093dcd2964686a
cjonasl <jonas5669@outlook.com>
2019-07-11 09:23:30 +0100

    Commit 2019-07-11 kl. 09:23

---
+++ Work/Fixzone/Tasks/Task1/Task39_Day68_Date190709_Complete customer search client connect/Script.sql
@@ -0,0 +1,8 @@
+INSERT INTO ProcessHeader(ProcessID, ProcessName, ProcessAccessLevel)
+VALUES(32, 'CustAdvShr', 1)
+
+INSERT INTO ProcessDetail(ProcessID, ProcessStep, ExecuteLinkType, ExecuteLinkID, CustomerProcess, ClientId)
+VALUES(32, 1, 1, 73, 0, NULL)
+
+INSERT INTO ProcessDetail(ProcessID, ProcessStep, ExecuteLinkType, ExecuteLinkID, CustomerProcess, ClientId)
+VALUES(32, 2, 1, 70, 0, NULL)
